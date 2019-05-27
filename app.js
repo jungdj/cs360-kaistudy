312e14083dee4708e0815db19028a4a40ab9da91
cookie <jung_dj@kaist.ac.kr>
2019-05-28 01:53:58 +0900

    fix access control

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(morgan('dev', {
 }));
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://52.78.179.78/")
+  res.header("Access-Control-Allow-Origin", "http://52.78.179.78")
   res.header("Access-Control-Allow-Headers", "Authorization, X-Requested-With, Content-Type, Accept")
   res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE,OPTIONS,PATCH')
   res.header("Access-Control-Allow-Credentials", true)
