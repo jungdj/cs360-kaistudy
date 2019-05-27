371f0d509fa3f284030a6fc82cee62c0ff6a7f26
cookie <jung_dj@kaist.ac.kr>
2019-05-28 01:46:53 +0900

    category name unique

--- migrations/20190512163209_create_category.js
+++ migrations/20190512163209_create_category.js
@@ -2,7 +2,7 @@
 exports.up = function(knex, Promise) {
 	return knex.schema.createTable('category', function (t) {
 		t.increments('category_id').notNullable().unique().primary()
-		t.string('name', 45).notNullable()
+		t.string('name', 45).notNullable().unique()
 		t.dateTime('created_at').notNullable().defaultTo(knex.fn.now())
 		t.dateTime('updated_at').notNullable().defaultTo(knex.fn.now())
 	})
